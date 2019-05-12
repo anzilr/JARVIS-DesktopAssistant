9971796dcbca7154bdefb5d5e62779c933c08d15
anzilr <anzilraheem@gmail.com>
2019-05-12 10:40:20 +0530

    Initialized

--- config.py
+++ config.py
@@ -2,15 +2,15 @@
 # set your credentials here
 
 import wolframalpha
-FBEMAIL = "anfalrahim3@gmail.com" # facebook email id REQUIRED
+FBEMAIL = "jarvisai@gmail.com" # facebook email id REQUIRED
 
-FBPASSWORD = "anfal786" # facebook password REQUIRED
+FBPASSWORD = "jarvisai" # facebook password REQUIRED
 
-account_sid = 'ACe28c8e59e9316b6b1eb4dc100f3cbb9c' # twilo sid for sms service
+account_sid = 'bdbcsdfisfhdghihghsduhgoseh' # twilo sid for sms service
 
-auth_token = 'd63790b7c989b578f61d875a5e5c87f4' # twilo token for sms service
+auth_token = 'kihfhiruhihrir;isghghsghoh' # twilo token for sms service
 
-wclient = wolframalpha.Client('R789KG-LGUH9GR33E') # wolframalpha token for web services "REQUIRED"
+wclient = wolframalpha.Client('bferrigirihghr') # wolframalpha token for web services "REQUIRED"
 
 music_dir = "E:\Music" # path to your music directory. INSTALL AIMP3 FOR THIS FEATURE.
 
